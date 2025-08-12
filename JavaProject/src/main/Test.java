1b0b28e057920a8b32dcb48b354b04b1cd96770c
VaishnaviMane26 <139062530+VaishnaviMane26@users.noreply.github.com>
2025-08-12 19:00:02 +0530

    My Second Commit

--- JavaProject/src/main/Test.java
+++ JavaProject/src/main/Test.java
@@ -8,6 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		TeamService service=new TeamService();
         service.printTeamdetails();
+        System.out.println("test github");
 	}
 
 }
